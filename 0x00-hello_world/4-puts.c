53a38c143c685c4bd7d5fa8bc0cb57372aaa3976
Rasheedat71 <106766855+Rasheedat71@users.noreply.github.com>
2022-07-01 02:05:48 +0100

    Successful

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
-*Description: main - Prints out a string to stdout.
-*Return: 0 if success
-*/
+ * main - entry point
+ *
+ * Return: 0 after printing the function
+ */
 int main(void)
 {
-puts("\"Programming is like building a multiligual puzzle);
-return (o);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
