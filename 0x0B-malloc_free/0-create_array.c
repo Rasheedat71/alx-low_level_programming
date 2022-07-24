3a9c08dc73df287d96ed104075443605b52a43b8
Rasheedat71 <yrasheedat12@gmail.com>
2022-07-24 15:31:07 -0700

    Sting

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * create_array - creates an array of char and
+ * initializes it with a specific char
+ * @size: size of the array
+ * @c: the specific char to initialize the array with
+ * Return: If size == 0 or the function fails - NULL.
+ * Otherwise - a pointer to the array.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+char *array;
+unsigned int i;
+
+if (size == 0)
+return (NULL);
+array = malloc(sizeof(c) * size);
+if (array == NULL)
+return (NULL);
+for (i = 0; i < size; i++)
+array[i] = c;
+return (array);
+}
