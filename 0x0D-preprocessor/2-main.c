860129c416206d8419c1382b783638e791ab652e
Rasheedat Olufunke Yusuf <106766855+Rasheedat71@users.noreply.github.com>
2022-08-01 21:39:47 +0100

    Changes

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Prints the name of the file program
- *        was compiled from, followed by a new line.
- * Return: Always 0.
+ * main - prints the name of the file program was compiled from
+ * Return: Always 0
  */
 
 int main(void)
