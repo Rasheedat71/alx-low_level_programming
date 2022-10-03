c727424171c4210109e3ad9acf3d8211ef74793e
Rasheedat Olufunke Yusuf <106766855+Rasheedat71@users.noreply.github.com>
2022-10-03 23:02:09 +0100

    file

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://raw.github.com/Rasheedat71/alx-low_level_programming/main/0x18-dynamic_libraries/preload (1).so
-export LD_PRELOAD=/tmp/preload (1).so
+wget -P /tmp/ https://raw.github.com/Natzsmart/alx-low_level_programming/main/0x18-dynamic_libraries/preload.so
+export LD_PRELOAD=/tmp/preload.so
